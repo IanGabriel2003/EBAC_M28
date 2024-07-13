11e54fbfc91e22bfa3587cfffaea496abe19cb08
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2024-07-12 21:33:03 -0300

    Estilização do input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 
 import Perfil from "./components/Perfil";
-import Formulario from "./components/formulario";
+import Formulario from "./components/Formulario";
 import ReposList from "./components/RepoList";
 
 function App() {
   const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true)
-  const [nomeUsuario, setNomeUsuario] = useState()
+  const [nomeUsuario, setNomeUsuario] = useState('')
 
   return(
     <>
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)}/>
+      <input className="input" placeholder="Digite o úsuario" type="text" onBlur={(e) => setNomeUsuario(e.target.value)}/>
 
       {nomeUsuario.length > 4 && (
         <>
